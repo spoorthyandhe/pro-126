d751f54b059f1ae0e685f3b68f067cfb24e28eba
spoorthyandhe <70134338+spoorthyandhe@users.noreply.github.com>
2022-06-21 10:18:43 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+import React from "react"
+import PickImage from "./Realtime-Image-detection-master/screens/Camera.js"
+
+export default class App extends React.Component{
+    render(){
+        return <PickImage/>
+    }
+}
